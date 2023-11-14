9b74fe1f4442a54ca4f768e0412f147d2cc61c7b
Elisei Smirnov <61423871+kezouke@users.noreply.github.com>
2023-11-14 11:10:57 +0300

    Update ex1.sh

--- week10/ex1.sh
+++ week10/ex1.sh
@@ -3,6 +3,9 @@
 
 mkdir tmp
 
+# Store the current working directory in a variable
+current_dir=$(pwd)
+
 # Compile monitor.c
 gcc -o monitor monitor.c
 
@@ -10,17 +13,21 @@ gcc -o monitor monitor.c
 gcc -o ex1 ex1.c
 
 # Run monitor.c in the background
-./monitor /home/kezouke/week10/tmp &
+./monitor "$current_dir/tmp" &
 
 sleep 1
 
 # Run ex1.c
-gnome-terminal -- bash -c "./ex1 /home/kezouke/week10/tmp; exec bash"
+gnome-terminal -- bash -c "./ex1 $current_dir/tmp; exec bash"
 
-cd ~/week10/tmp
-bash ~/week10/ex1_test.sh
+# Change directory to tmp
+cd "$current_dir/tmp"
 
-cd ../
+# Run ex1_test.sh
+bash "$current_dir/ex1_test.sh"
+
+# Change directory back to the original directory
+cd "$current_dir"
 
 # Terminate monitor.c gracefully with Ctrl+C
 kill -INT %%
